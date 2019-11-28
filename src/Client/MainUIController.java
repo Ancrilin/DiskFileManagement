2e6c34c12b2133d254234ed73d28e4602b1ea1cb
974357963@qq.com <DZY@DZY-PC>
2019-11-28 23:06:39 +0800

    FXML

---
+++ src/Client/MainUIController.java
@@ -0,0 +1,32 @@
+package Client;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
+public class MainUIController {
+
+    @FXML
+    private GridPane GridPane;
+
+    @FXML
+    private TreeView<?> TreeView;
+
+    @FXML
+    private MenuBar MenuBar;
+
+    @FXML
+    private AnchorPane AnchorPane;
+
+    @FXML
+    private ScrollBar ScrollBar;
+    
+    private void iniFilrTree()
+    {
+    	
+    }
+
+}
